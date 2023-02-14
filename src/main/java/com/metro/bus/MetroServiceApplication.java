4a226bc896686e296587461505e276e16982e233
satyam <way2satyam.k@gmail.com>
2023-02-15 00:35:44 +0530

    Initial Commit

---
+++ src/main/java/com/metro/bus/MetroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.metro.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metro/bus/MetroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metro.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
